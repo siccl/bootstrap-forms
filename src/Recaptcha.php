a6c0083da2d98839c962389824fc616eca18c81d
Hansel Gaete <neoroot@sic.cl>
2019-02-22 09:11:38 -0300

    GroupId for ReCaptcha

--- src/Recaptcha.php
+++ src/Recaptcha.php
@@ -6,14 +6,15 @@ class Recaptcha{
     private $public_key;
     private $private_key;
 
-    function __construct($public_key=null,$private_key=null,$version=2){
+    function __construct($public_key=null,$private_key=null,$version=2,$groupId=null){
         $this->public_key=$public_key;
         $this->private_key=$private_key;
         $this->google_recaptcha_version=$version;
+        $this->groupId=$groupId;
     }
 
     public function html(){
-        return '<div class="form-group">
+        return '<div class="form-group" '.($this->groupId?'id="'.$this->groupId.'"':'').'>
           <div class="g-recaptcha" data-sitekey="'.$this->public_key.'"></div>
         </div>';
     }
