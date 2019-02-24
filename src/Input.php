891cbf512d6a40cb742d80994417fe0e6bcf4fd7
Hansel Gaete <neoroot@sic.cl>
2019-02-24 11:20:02 -0300

    Feather final Fix

--- src/Input.php
+++ src/Input.php
@@ -1,15 +1,14 @@
 <?php
 // New Changes
 namespace Teknicode\Form;
-use Feather\Icons;
 class Input{
     private $attributes = array("class"=>"form-control");
     public $width;
-
+    
     function __construct($width){
         $this->width=$width;
     }
-
+    
     public function set(){
         if(func_num_args() == 2) {
             $key = func_get_arg(0);
@@ -26,33 +25,33 @@ class Input{
         }
         return $this;
     }
-
+    
     public function get($key){
         return (isset($this->attributes[$key]) ? $this->attributes[$key] : null);
     }
-
+    
     public function html(){
         $atts = '';
         if ($this->get('type')=='hidden'){
             return '<input type="hidden" '.
-                    ($this->get('name')?'name="'.$this->get('name').'" ':'').
-                    ($this->get('value')?'value="'.$this->get('value').'" ':'').
-                    ($this->get('id')?'id="'.$this->get('id').'" ':'id="'.$this->get('name').'" ').
-                    '>'."\n";
+                ($this->get('name')?'name="'.$this->get('name').'" ':'').
+                ($this->get('value')?'value="'.$this->get('value').'" ':'').
+                ($this->get('id')?'id="'.$this->get('id').'" ':'id="'.$this->get('name').'" ').
+                '>'."\n";
         }
         $return = '<div class="form-group input-group '. $this->get('groupClass') .'" '.($this->get('groupId')? ' id='. $this->get('groupId') :'').'>';
         if ($this->get('label')!=""){
             $return .= ($this->get('label')?'<label'.($this->get('id')? ' for="'.$this->get('id').'"':'').'>'.$this->get('label').'</label> ':'');
         }elseif ($this->get('addon-feather')!=""){
-            //$return .= '<span class="input-group-text"><i data-feather="' . $this->get('addon-feather') . '"></i></span>';
-            $icons = new Feather\Icons;
-            $return .= '<span class="input-group-text">' . $icons->get($this->get('addon-feather')) . '</span>';
-        }        
+            $icons = new \Feather\Icons;
+            $icon = $icons->get($this->get('addon-feather'),[],false);
+            $return .= '<span class="input-group-text">' . $icon . '</span>';
+        }
         //input-group-addon
         if(in_array($this->get('type'),["radio","checkbox"])){
             $this->set("class",str_replace("form-control","",$this->get('class')));
         }
-
+        
         unset($this->attributes['label']);
         foreach($this->attributes as $attribute => $value){
             if(!in_array($attribute,["options","addon-feather","groupClass"])) {
@@ -60,7 +59,7 @@ class Input{
                 $atts .= (!empty($atts) ? ' ' : '') . $attribute . '="' . $value . '" ';
             }
         }
-
+        
         if($this->get('type') == "textarea"){
             $return .= '<textarea '.$atts.'>'.$this->get('value').'</textarea>';
         }elseif($this->get('type') == "radio"){
@@ -77,5 +76,5 @@ class Input{
         $return .= '</div>'."\n";
         return $return;
     }
-
+    
 }
