cf1752bb2d108c93c2a5068057f89cf1f3cad352
Hansel Gaete <neoroot@sic.cl>
2019-02-26 18:51:00 -0300

    Select Changes
    Input Changes to Select
    Enable pass $dbh PDO to Select

--- src/Form.php
+++ src/Form.php
@@ -5,7 +5,13 @@ class Form{
     private $inputs=[];
     private $form;
     private $layout;
-
+    protected $dbh;
+    function __construct($dbh=""){
+        if (version_compare(phpversion(), '5.6.0', '<')) {
+            die('PHP 5.6.0 required for PHPAuth engine!');
+        }
+        $this->dbh = $dbh;
+    }
     public function open(){
         $args=func_get_args()[0];
         $html = '<form action="'.(empty($args['action'])? $_SERVER['REQUEST_URI'] : $args['action']).'" method="'.(empty($args['method'])? "post" : $args['method']).'" ';
@@ -18,33 +24,33 @@ class Form{
         $html .= '>'."\n";
         $this->form = $html;
     }
-
+    
     public function layout($columns){
         if(!isset($this->layout)){
             $this->layout = new Layout($columns);
         }
         return $this->layout;
     }
-
+    
     public function input($width=12){
         $i = new Input($width);
         $this->inputs[]=$i;
         return $i;
     }
-
+    
     public function select($width=12){
-        $i = new Select($width);
+        $i = new Select($width,$this->dbh);
         $this->inputs[]=$i;
         return $i;
     }
-
+    
     public function recaptcha($public_key,$private_key=null,$version=2,$groupId=null){
         //todo: use private key here when form and process are merged
         $i = new Recaptcha($public_key,$private_key,$version,$groupId);
         $this->inputs[]=$i;
         return $i;
     }
-
+    
     public function html($width=12,$content=null){
         $i = new \stdClass();
         $i->width = $width;
@@ -52,21 +58,21 @@ class Form{
         $this->inputs[]=$i;
         return $i;
     }
-
+    
     public function button($width=12){
         $i = new Button($width);
         $this->inputs[]=$i;
         return $i;
     }
-
+    
     public function compile(){
         $this->_compile();
-
+        
         $this->form .= '</form>'."\n";
-
+        
         return $this->form;
     }
-
+    
     private function _compile(){
         foreach($this->inputs as $input){
             //$this->form .= '<div class="col-md-'.$input->width.'">';
--- src/Select.php
+++ src/Select.php
@@ -3,11 +3,13 @@ namespace Teknicode\Form;
 class Select{
     private $attributes = array("class"=>"form-control");
     public $width;
-
-    function __construct($width=12){
-        $this->width=$width;
+    protected $dbh;
+    function __construct($width, $dbh=""){
+        if (version_compare(phpversion(), '5.6.0', '<')) {
+            die('PHP 5.6.0 required for PHPAuth engine!');
+        }
+        $this->dbh = $dbh;
     }
-
     public function set(){
         if(func_num_args() == 2) {
             $key = func_get_arg(0);
@@ -24,37 +26,57 @@ class Select{
         }
         return $this;
     }
-
+    
     public function get($key){
         return (isset($this->attributes[$key]) ? $this->attributes[$key] : null);
     }
-
+    
     public function html(){
         $html = '';
-
-        $return = '<div class="form-group">'.($this->get('label')?'<label'.($this->get('id')? ' for="'.$this->get('id').'"':'').'>'.$this->get('label').'</label>':'');
-
+        
+        //$return = '<div class="form-group">'.($this->get('label')?'<label'.($this->get('id')? ' for="'.$this->get('id').'"':'').'>'.$this->get('label').'</label>':'');
+        $return = '<div class="form-group input-group '. $this->get('groupClass') .'" '.($this->get('groupId')? ' id='. $this->get('groupId') :'').'>';
+        if ($this->get('label')!=""){
+            $return .= ($this->get('label')?'<label'.($this->get('id')? ' for="'.$this->get('id').'"':'').'>'.$this->get('label').'</label> ':'');
+        }elseif ($this->get('addon-feather')!=""){
+            $icons = new \Feather\Icons;
+            $icon = $icons->get($this->get('addon-feather'),[],false);
+            $return .= '<span class="input-group-text">' . $icon . '</span>';
+        }
         unset($this->attributes['label']);
         foreach($this->attributes as $attribute => $value){
-            if(!in_array($attribute,["options","value"])){
+            if(!in_array($attribute,["options","value","query"])){
                 $html .= (!empty($html)?' ':'').$attribute.'="'.$value.'"';
             }
         }
-
+        
         $return .= '<select '.$html.'>';
         $group_open = false;
-        foreach($this->get('options') as $label => $value){
-            if($value === "--group--"){
-                $return .= ($group_open == true ? '</optgroup>' : '').'<optgroup label="'.$label.'">';
-                $group_open=true;
-            }elseif( is_array($value) ){
-                $return .= '<option ';
-                foreach( $value as $att => $val ){
-                    $return .= $att.'="'.$val.'" '.($att == "value" && $val == $this->get('value') ? 'selected="selected" ' : '');
+        if ($this->get('options')!=""){
+            foreach($this->get('options') as $label => $value){
+                if($value === "--group--"){
+                    $return .= ($group_open == true ? '</optgroup>' : '').'<optgroup label="'.$label.'">';
+                    $group_open=true;
+                }elseif( is_array($value) ){
+                    $return .= '<option ';
+                    foreach( $value as $att => $val ){
+                        $return .= $att.'="'.$val.'" '.($att == "value" && $val == $this->get('value') ? 'selected="selected" ' : '');
+                    }
+                    $return .= '>' . $label . '</option>';
+                }else{
+                    $return .= '<option value="' . $value . '"'.($value == $this->get('value') ? ' selected="selected"' : '').'>' . $label . '</option>';
                 }
-                $return .= '>' . $label . '</option>';
-            }else{
-                $return .= '<option value="' . $value . '"'.($value == $this->get('value') ? ' selected="selected"' : '').'>' . $label . '</option>';
+            }
+        }
+        if ($this->get('query')!=""){
+            $query = $this->dbh->prepare($this->get('query'));
+            $query->execute();
+            if ($query->rowCount() == 0) {
+                $data = [];
+            }
+            //$data = $query->fetch(\PDO::FETCH_ASSOC);
+            while( $row = $query->fetch()) {
+                $return .= '<option value="' . $row[1] . '"'.($row[1] == $this->get('value') ? ' selected="selected"' : '').'>' . $row[0] . '</option>';
             }
         }
         $return .= '</select>
